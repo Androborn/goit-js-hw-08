23bc5a325b526f3166379a896939909e873d90ad
androborn <andrii.titov@gmail.com>
2021-10-31 04:19:14 +0200

    Add tasks #2 and #3 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+// В HTML есть <iframe> с видео для Vimeo плеера. Напиши скрипт который будет сохранять текущее время воспроизведения видео в локальное хранилище и, при перезагрузке страницы, продолжать воспроизводить видео с этого времени.
 
+// Выполняй это задание в файлах 02-video.html и 02-video.js. Разбей его на несколько подзадач:
+// 1. Ознакомься с документацией библиотеки Vimeo плеера.
+// 2. Добавь библиотеку как зависимость проекта через npm.
+// 3. Инициализируй плеер в файле скрипта как это описано в секции pre-existing player, но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
+// 4. Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
+// 5. Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища будет строка "videoplayer-current-time".
+// 6. При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
+// 7. Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframePlayer = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframePlayer);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(savePlayTime, 1000));
+
+function savePlayTime({ seconds }) {
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}
+
+if (LOCALSTORAGE_KEY) {
+  player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+// В HTML есть разметка формы. Напиши скрипт который будет сохранять значения полей в локальное хранилище когда пользователь что-то печатает.
 
+// Выполняй это задание в файлах 03-feedback.html и 03-feedback.js. Разбей его на несколько подзадач:
+// 1. Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message, в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
+// 2. При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
+// 3. При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
+// 4. Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
+
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const formEmail = document.querySelector('[name="email"]');
+const formMessage = document.querySelector('[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+let formInput = {};
+
+form.addEventListener('input', throttle(saveFormInput, 500));
+form.addEventListener('submit', submitHandler);
+
+checkAndSetSavedFormInput();
+
+function saveFormInput(event) {
+  formInput[event.target.name] = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formInput));
+}
+
+function checkAndSetSavedFormInput() {
+  const storedValues = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (storedValues) {
+    const { email, message } = JSON.parse(storedValues);
+
+    formEmail.value = email;
+    formMessage.value = message;
+
+    formInput = JSON.parse(storedValues);
+  }
+}
+
+function submitHandler(event) {
+  event.preventDefault();
+
+  console.log(formInput);
+  event.target.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
